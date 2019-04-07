65749873a7b9e43bf6b02697a06e73901d5b1c5a
nzws <git@nzws.me>
2019-04-07 20:08:19 +0900

    まあ多分完成した

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,80 @@
 import { h, app } from 'hyperapp';
 import '../style/index.scss';
 
+const domParser = new DOMParser();
+
+const INSTANCE_DOMAIN = 'friends.nico';
+
 const state = {
   toots: []
 };
 
 const actions = {
-  connect: () => {
-    const ws = new WebSocket(`wss://friends.nico/api/v1/streaming/public/local`);
-    let heartbeat;
+  add: (payload, use_push = false) => {
+    payload.content = domParser.parseFromString(payload.content, 'text/html').documentElement.textContent;
+    if (!payload.content) return state; // 画像のみは弾く
 
-    ws.onopen = () => {
-      heartbeat = setInterval(() => ws.send('ping'), 5000);
+    if (!payload.account.display_name) {
+      payload.account.display_name = payload.account.username;
+    }
 
-      ws.onmessage =
-        config.live.page === 'livepage'
-          ? livepage_comment.onmessage
-          : comment_viewer.onmessage;
+    if (use_push) {
+      state.toots.push(payload);
+    } else {
+      state.toots.unshift(payload);
+    }
 
-      ws.onclose = () => {
-        kit.elemId('err_comment').className = '';
-      };
-    };
+    if (state.toots.length > 30) {
+      state.toots.pop();
+    }
+
+    return state;
   }
 };
 
 const view = (state, actions) => (
   <main>
-    <div class="list">
-      {state.toots.map(v => (
-        <span class="box" onclick={() => actions.play(v.file)}>{v.say}</span>
-      ))}
-    </div>
+    {state.toots.map(v => (
+      <div class="toot">
+        <div class="display_name">{v.account.display_name}</div>
+        {v.content}
+      </div>
+    ))}
   </main>
 );
 
-app(state, actions, view, document.body);
+const knzk = app(state, actions, view, document.body);
+
+fetch(`https://${INSTANCE_DOMAIN}/api/v1/timelines/public?local=true`,{
+  headers: { 'content-type': 'application/json' },
+  method: 'GET'
+}).then(response => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw response;
+  }
+}).then(json => {
+  json.forEach(toot => {
+    knzk.add(toot, true);
+  });
+
+  const ws = new WebSocket(`wss://${INSTANCE_DOMAIN}/api/v1/streaming?stream=public:local`);
+
+  ws.onopen = () => {
+    setInterval(() => ws.send('ping'), 5000);
+
+    ws.onmessage = (data) => {
+      const resdata = JSON.parse(data.data);
+
+      if (resdata.event === 'update') {
+        const payload = JSON.parse(resdata.payload);
+        knzk.add(payload);
+      }
+    };
+
+    ws.onclose = () => {
+      location.reload();
+    };
+  };
+});
